1b195f4052fc2f96d1b3a13aae3b53abd634b28c
MaximG <33728489+MaximGonnissen@users.noreply.github.com>
2023-10-23 17:11:10 +0200

    Add estimate calculation

--- MaximGonnissen/progress_bar.py
+++ MaximGonnissen/progress_bar.py
@@ -26,14 +26,15 @@ class ProgressBar:
     """
 
     def __init__(self, collection: Collection, bar_length: int = 20, show_count: bool = True,
-                 clear_when_done: bool = True, full_bar: str = '█', half_bar: str = '▌', empty_bar: str = '░',
-                 bar_start: str = '[', bar_end: str = ']', ansi_colour: str = None,
+                 clear_when_done: bool = True, show_estimate: bool = True, full_bar: str = '█', half_bar: str = '▌',
+                 empty_bar: str = '░', bar_start: str = '[', bar_end: str = ']', ansi_colour: str = None,
                  parent_progress_bar: 'ProgressBar' = None):
         """
         Create a new progress bar.
         :param bar_length: Length of the progress bar.
         :param show_count: Whether to show the current count.
         :param clear_when_done: Whether to clear the progress bar when done.
+        :param show_estimate: Whether to show the estimated time remaining.
         :param full_bar: Character to use for a full bar.
         :param half_bar: Character to use for a half bar.
         :param empty_bar: Character to use for an empty bar.
@@ -46,6 +47,7 @@ class ProgressBar:
         self.bar_length = bar_length
         self.show_count = show_count
         self.clear_when_done = clear_when_done
+        self.show_estimate = show_estimate
         self.full_bar = full_bar
         self.half_bar = half_bar
         self.empty_bar = empty_bar
@@ -53,6 +55,11 @@ class ProgressBar:
         self.bar_end = bar_end
         self.progress_bar_colour = ansi_colour
         self.parent_progress_bar = parent_progress_bar
+
+        self.children_progress_bars = []
+
+        if self.parent_progress_bar is not None:
+            self.parent_progress_bar.children_progress_bars.append(self)
 
         self.count = 0
         self.total = len(collection)
@@ -66,6 +73,22 @@ class ProgressBar:
             return self.parent_progress_bar.get_update_string() + self.parent_progress_bar.nested_padding() + '\t'
         else:
             return ''
+
+    def get_progress(self) -> float:
+        """
+        Get the progress.
+        :return: Progress.
+        """
+        return self.count / self.total
+
+    def get_estimated_time_remaining(self) -> float:
+        """
+        Get the estimated time remaining.
+        :return: Estimated time remaining.
+        """
+        if self.count == 0:
+            return 0
+        return ((time.time() - self.start_time) / self.count) * (self.total - self.count)
 
     def __enter__(self):
         self.count = 0
@@ -104,6 +127,9 @@ class ProgressBar:
         update_string += f' ({progress * 100:.2f}%)'
 
         update_string += f' {time.time() - self.start_time:.2f}s'
+
+        if self.show_estimate:
+            update_string += f' T-{self.get_estimated_time_remaining():.2f}s'
 
         update_string += ASCIIColour.RESET if self.progress_bar_colour is not None else ''
 
@@ -144,6 +170,6 @@ if __name__ == '__main__':
 
     with ProgressBar(ExampleCollection(100)) as pb:
         for i in pb:
-            with ProgressBar(ExampleCollection(30000), parent_progress_bar=pb) as pb2:
+            with ProgressBar(ExampleCollection(30000), parent_progress_bar=pb, show_estimate=False, show_count=False) as pb2:
                 for j in pb2:
                     pass
