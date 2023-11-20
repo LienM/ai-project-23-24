4249dca302b7d911fe1a5eb4beef24e5091350a4
s0196084@ad.ua.ac.be <vernieuwefelix@gmail.com>
2023-11-20 21:31:59 +0100

    fix: some tiny fixes

--- FelixVernieuwe/03 - Research Question 1/features/price_sensitivity.py
+++ FelixVernieuwe/03 - Research Question 1/features/price_sensitivity.py
@@ -4,7 +4,7 @@ import pandas as pd
 def price_sensitivity_feature(transactions: pd.DataFrame, customers: pd.DataFrame):
     customer_price_ranges = transactions.groupby('customer_id')['price'].agg(['min', 'max'])
     customer_price_ranges['diff'] = customer_price_ranges['max'] - customer_price_ranges['min']
-    customer_price_ranges['price_sensitivity'] = pd.qcut(customer_price_ranges['diff'], 5, labels=[0, 1, 2, 3, 4])
+    customer_price_ranges['price_sensitivity'] = pd.qcut(customer_price_ranges['diff'], 5, labels=[0, 1, 2, 3, 4], duplicates='drop')
 
     customer_price_ranges['purchase_count'] = transactions['customer_id'].value_counts()
     customer_price_ranges.loc[customer_price_ranges['purchase_count'] < 10, 'price_sensitivity'] = 0
