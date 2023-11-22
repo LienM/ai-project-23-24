1e2eabc465af67be6283caf760b7ed733138b15b
MaximG <33728489+MaximGonnissen@users.noreply.github.com>
2023-11-22 03:52:29 +0100

    Add gendered method to main

--- MaximGonnissen/main.py
+++ MaximGonnissen/main.py
@@ -1,88 +1,46 @@
-from utils.kaggle_tool import KaggleTool
-from analysis.seasonality_analysis import run_seasonal_analysis
-from utils.utils import DataFileNames, get_data_path
-import time
-import zipfile
 import multiprocessing as mp
-import json
+
+from analysis.seasonality_analysis import run_seasonal_analysis_parallel, run_seasonal_analysis
+from features.add_gender import add_gender
+from pruning.prune_inactive import prune_inactive
+from pruning.prune_no_purchases import prune_no_purchases
+from utils.utils import load_data_from_hnm, DataFileNames
 
 
-def already_ran_for(score_offset: int, day_range: int, kaggle_tool: KaggleTool):
-    submissions = kaggle_tool.list_submissions_wrapped()
-
-    # Check if submission with same parameters already exists
-    for submission in submissions:
-        try:
-            description = submission.parse_json_description()
-        except json.decoder.JSONDecodeError:
-            continue
-        if description['max_score_offset'] == score_offset and description['max_score_day_range'] == day_range:
-            return True
-
-    return False
-
-
-def get_combinations():
+def get_seasonality_combinations():
     max_score_offset_range = range(-60, 10, 1)
     max_score_day_range_range = range(7, 30, 1)
 
-    combinations = [(max_score_offset, max_score_day_range) for max_score_offset in max_score_offset_range for max_score_day_range in max_score_day_range_range]
+    combinations = [(max_score_offset, max_score_day_range) for max_score_offset in max_score_offset_range for
+                    max_score_day_range in max_score_day_range_range]
     return combinations
 
 
-def _run_seasonal_analysis(max_score_offset: int, max_score_day_range: int):
-    kaggle_tool = KaggleTool('h-and-m-personalized-fashion-recommendations')
-
-    if already_ran_for(max_score_offset, max_score_day_range, kaggle_tool):
-        print(
-            f'> Already ran analysis for max_score_offset={max_score_offset} and max_score_day_range={max_score_day_range}.')
-        return
-
-    print(
-        f'> Running analysis for max_score_offset={max_score_offset} and max_score_day_range={max_score_day_range}.')
-
-    start_time = time.time()
-    output_bytes = run_seasonal_analysis(max_score_offset, max_score_day_range, True, verbose=False, to_csv=False)
-
-    print(
-        f'> Finished analysis for max_score_offset={max_score_offset} and max_score_day_range={max_score_day_range} in {time.time() - start_time} seconds.')
-
-    print(f'> Zipping output')
-    zip_path = get_data_path() / DataFileNames.OUTPUT_DIR / DataFileNames.ZIP_DIR / f'seasonal_analysis_{max_score_offset}_{max_score_day_range}.zip'
-
-    if not zip_path.parent.exists():
-        zip_path.parent.mkdir(parents=True)
-
-    with zipfile.ZipFile(zip_path, 'w') as zip_file:
-        zip_file.writestr('seasonal_analysis.csv', output_bytes.getvalue())
-
-    print(f'> Uploading output to Kaggle')
-    metadata = {'max_score_offset': max_score_offset, 'max_score_day_range': max_score_day_range, }
-    kaggle_tool.upload_submission(zip_path, metadata=metadata)
-
-    return
-
-
-def run_seasonal_analysis_parallel(_mp_pool_count: int):
-    """
-    Runs seasonal analysis for a range of parameters and uploads the results to Kaggle.
-    Processing is done in parallel using multiprocessing.
-    :param _mp_pool_count: Number of processes to use for multiprocessing
-    """
-    with mp.Pool(_mp_pool_count) as pool:
-        pool.starmap(_run_seasonal_analysis, get_combinations())
-
-
-if __name__ == '__main__':
-    script_start_time = time.time()
+def seasonal_analysis():
     use_mp = False
 
     if use_mp:
         mp_pool_count = max(mp.cpu_count() - 1, 1)
         print(f'Using {mp_pool_count} cores for multiprocessing.')
 
-        run_seasonal_analysis_parallel(mp_pool_count)
+        run_seasonal_analysis_parallel(mp_pool_count, get_seasonality_combinations())
 
     else:
-        for combination in get_combinations():
-            _run_seasonal_analysis(*combination)
+        for combination in get_seasonality_combinations():
+            run_seasonal_analysis(*combination)
+
+
+def get_gendered_customers_df():
+    customers_df = load_data_from_hnm(DataFileNames.CUSTOMERS)
+    customers_df = prune_inactive(customers_df)
+
+    transactions_df = load_data_from_hnm(DataFileNames.TRANSACTIONS_TRAIN)
+    customers_df = prune_no_purchases(customers_df, transactions_df)
+
+    articles_df = load_data_from_hnm(DataFileNames.ARTICLES)
+
+    return add_gender(customers_df, transactions_df, articles_df)
+
+
+if __name__ == '__main__':
+    print(get_gendered_customers_df().head(100))
