abe76bef155324976c9d56aee914c759054d3b11
MaximG <33728489+MaximGonnissen@users.noreply.github.com>
2023-12-05 23:38:09 +0100

    Refactor method name

--- MaximGonnissen/features/add_age_group.py
+++ MaximGonnissen/features/add_age_group.py
@@ -13,7 +13,7 @@ class AgeGroup:
     OLD = '60+'
 
 
-def add_age_group_feature(customers_df: pd.DataFrame):
+def add_age_group(customers_df: pd.DataFrame):
     """
     Adds age group features to the dataframe, using the age column as origin.
     :param customers_df: Customers dataframe to add features to.
