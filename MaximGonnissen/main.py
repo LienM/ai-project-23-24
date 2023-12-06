e08503c8db77f587a5b72628149ca3bac31fc009
MaximG <33728489+MaximGonnissen@users.noreply.github.com>
2023-12-06 01:54:50 +0100

    Add gender recommendation generation

--- MaximGonnissen/main.py
+++ MaximGonnissen/main.py
@@ -1,10 +1,14 @@
 import multiprocessing as mp
 
 from analysis.seasonality_analysis import run_seasonal_analysis_parallel, run_seasonal_analysis
+from pruning.prune_outdated_items import prune_outdated_items
+from selectors.get_most_popular_gendered_items import get_most_popular_gendered_items
+from utils.utils import load_data_from_hnm, DataFileNames, get_data_path
+from utils.convert_to_parquet import convert_to_parquet
 from features.add_gender import add_gender
-from pruning.prune_inactive import prune_inactive
-from pruning.prune_no_purchases import prune_no_purchases
-from utils.utils import load_data_from_hnm, DataFileNames
+
+from io import BytesIO
+import zipfile
 
 
 def get_seasonality_combinations():
@@ -30,17 +34,46 @@ def seasonal_analysis():
             run_seasonal_analysis(*combination)
 
 
-def get_gendered_customers_df():
-    customers_df = load_data_from_hnm(DataFileNames.CUSTOMERS)
-    customers_df = prune_inactive(customers_df)
+def generate_gender_recommendations():
+    articles_df = load_data_from_hnm(DataFileNames.ARTICLES.replace('.csv', '.parquet'), True, dtype={'article_id': str})
+    transactions_df = load_data_from_hnm(DataFileNames.TRANSACTIONS_TRAIN.replace('.csv', '.parquet'), True, dtype={'article_id': str})
+    sample_submission_df = load_data_from_hnm(DataFileNames.SAMPLE_SUBMISSION.replace('.csv', '.parquet'))
+    customers_df = load_data_from_hnm(DataFileNames.CUSTOMERS.replace('.csv', '.parquet'))
 
-    transactions_df = load_data_from_hnm(DataFileNames.TRANSACTIONS_TRAIN)
-    customers_df = prune_no_purchases(customers_df, transactions_df)
+    customers_df['gender'] = add_gender(customers_df, transactions_df, articles_df)
 
-    articles_df = load_data_from_hnm(DataFileNames.ARTICLES)
+    articles_df, transactions_df = prune_outdated_items(articles_df, transactions_df)
 
-    return add_gender(customers_df, transactions_df, articles_df)
+    most_popular_m_items = get_most_popular_gendered_items(articles_df, True)
+    most_popular_f_items = get_most_popular_gendered_items(articles_df, False)
+
+    submission_df = sample_submission_df.copy()
+
+    submission_df = submission_df.merge(customers_df[['customer_id', 'gender']], on='customer_id')
+    submission_df.loc[submission_df.gender == 'm', 'prediction'] = " ".join(most_popular_m_items)
+    submission_df.loc[submission_df.gender == 'u', 'prediction'] = " ".join(most_popular_m_items)
+    submission_df.loc[submission_df.gender == 'f', 'prediction'] = " ".join(most_popular_f_items)
+
+    submission_df.drop('gender', axis=1, inplace=True)
+
+    output_path = get_data_path() / DataFileNames.OUTPUT_DIR / DataFileNames.ZIP_DIR
+
+    submission_bytes = BytesIO()
+
+    submission_df.to_csv(submission_bytes, index=False)
+
+    with zipfile.ZipFile(output_path / 'submission.zip', 'w') as m_zip:
+        m_zip.writestr('submission.csv', submission_bytes.getvalue())
+
+
+def convert_all_to_parquet():
+    data_path = get_data_path() / DataFileNames.HNM_DIR
+
+    convert_to_parquet(data_path / DataFileNames.CUSTOMERS)
+    convert_to_parquet(data_path / DataFileNames.ARTICLES)
+    convert_to_parquet(data_path / DataFileNames.TRANSACTIONS_TRAIN)
+    convert_to_parquet(data_path / DataFileNames.SAMPLE_SUBMISSION)
 
 
 if __name__ == '__main__':
-    print(get_gendered_customers_df().head(100))
+    pass
