5ba5fe2cd3689eb0bd69fa349850dc9c1b01906b
MaximG <33728489+MaximGonnissen@users.noreply.github.com>
2023-12-29 17:22:37 +0100

    Clean up exploratory_analysis.py

--- MaximGonnissen/exploration/exploratory_analysis.py
+++ MaximGonnissen/exploration/exploratory_analysis.py
@@ -1,6 +1,7 @@
 import multiprocessing as mp
 import pathlib
 import time
+from typing import Optional
 
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -10,10 +11,19 @@ from PIL import Image
 from utils.utils import load_data, get_data_path, DataFileNames
 
 
-# Describe data
-def describe_data(df: pd.DataFrame, name: str, output_path_: pathlib.Path, thread_start_time: float = None) -> None:
+def describe_data(df: pd.DataFrame, name: str, output_path_: pathlib.Path,
+                  thread_start_time: Optional[float] = None) -> None:
+    """
+    Describe a dataframe and write the results to a markdown file.
+    :param df: Dataframe to describe
+    :param name: Name of the dataframe
+    :param output_path_: Path to write the markdown file to
+    :param thread_start_time: Time when the thread started, if applicable
+    :return:
+    """
     time_start = time.time()
     output_file = output_path_ / f'{name}_description.md'
+
     print(
         f'[ ] Describing data from {name}...{f" (thread started in {time.time() - thread_start_time:.2f} seconds)" if thread_start_time is not None else ""}')
 
@@ -53,17 +63,30 @@ def describe_data(df: pd.DataFrame, name: str, output_path_: pathlib.Path, threa
           f' ({time.time() - thread_start_time:.2f} seconds since thread start)' if thread_start_time is not None else '')
 
 
-# Describe images
 def add_image_info(image_path: pathlib.Path) -> dict:
+    """
+    Create a dictionary with the image name, width and height.
+    :param image_path: Path to the image
+    :return: Dictionary with the image name, width and height
+    """
     with Image.open(image_path) as image:
         width, height = image.size
         image_name = image_path.name
         return {'image': image_name, 'width': width, 'height': height}
 
 
-def describe_images(path: pathlib.Path, output_path_: pathlib.Path, mp_pool_count_: int = 1) -> None:
+def describe_images(path: pathlib.Path, output_path: pathlib.Path, mp_pool_count: int = 1) -> None:
+    """
+    Describe all images found recursively in the given path and write the results to a markdown file.
+    :param path: Path to the images
+    :param output_path: Path to write the markdown file to
+    :param mp_pool_count: Number of processes to use for multiprocessing
+    :return:
+    """
     time_start = time.time()
-    output_file = output_path_ / 'images_description.md'
+    if output_path.suffix != '.md':
+        output_path = output_path / 'images_description.md'
+
     print(f'[ ] Describing images...')
 
     subfolders = [subfolder for subfolder in path.glob('*') if subfolder.is_dir()]
@@ -75,10 +98,10 @@ def describe_images(path: pathlib.Path, output_path_: pathlib.Path, mp_pool_coun
     df = pd.DataFrame(columns=['image', 'width', 'height'])
     df = df.astype({'image': 'object', 'width': 'int64', 'height': 'int64'})
 
-    with mp.Pool(processes=mp_pool_count_) as pool_:
-        df = df._append(pool_.map(add_image_info, images), ignore_index=True)
+    with mp.Pool(processes=mp_pool_count) as pool:
+        df = df._append(pool.map(add_image_info, images), ignore_index=True)
 
-    with open(output_file, 'w') as f:
+    with open(output_path, 'w') as f:
         f.write(f'# Images\n\n')
         f.write(f'## Shape\n\n')
         f.write(f'Rows: {df.shape[0]}\n\n')
@@ -104,41 +127,41 @@ def describe_images(path: pathlib.Path, output_path_: pathlib.Path, mp_pool_coun
     print(f'[X] Described images in {time.time() - time_start:.2f} seconds.')
 
 
-def create_plot(df: pd.DataFrame, name: str, output_path_: pathlib.Path, combine_smallest: float = 0,
-                binwidth: float = 2, height: int = 5,
-                aspect: int = 2, plot_width: int = 15, plot_height: int = 10, rotation: float = 60, dpi: int = 400,
-                **kwargs) -> None:
+def create_plot(df: pd.DataFrame, name: str, output_path: pathlib.Path,
+                binwidth: float = 2, height: int = 5, aspect: int = 2, plot_width: int = 15, plot_height: int = 10,
+                rotation: float = 60, dpi: int = 400, **kwargs) -> None:
+    """
+    Create a plot for the given column in the given dataframe and save it to the given path.
+    :param df: Dataframe to create the plot for
+    :param name: Name of the column to create the plot for
+    :param output_path: Path to save the plot to
+    :param binwidth: Width of the bins
+    :param height: Height of the plot
+    :param aspect: Aspect ratio of the plot
+    :param plot_width: Width of the plot
+    :param plot_height: Height of the plot
+    :param rotation: Rotation of the x-axis labels
+    :param dpi: DPI of the plot
+    :param kwargs: Additional arguments to pass to the plot
+    """
     print(f'[ ] Creating plot for {name}...')
     df = df.copy()
-
-    if combine_smallest > 0:
-        # TODO: Fix this. Seems to replace a random value with the "Other" value.
-        total_count = df[name].count()
-        counts = df[name].value_counts().sort_values(ascending=False)
-        current_amount = 0
-        for index, count in counts.items():
-            if count / total_count > combine_smallest:
-                break
-            current_amount += count
-            if current_amount / total_count < combine_smallest:
-                df[name] = df[name].replace(index, f'Other ({combine_smallest * 100:.0f}%)')
-            else:
-                break
 
     sns.displot(data=df, x=name, binwidth=binwidth, height=height, aspect=aspect, **kwargs)
     plt.gcf().set_size_inches(plot_width, plot_height)
     plt.xticks(rotation=rotation)
     plt.tight_layout()
-    plt.savefig(output_path_ / f'{name}.png', dpi=dpi, bbox_inches='tight', **kwargs)
+    plt.savefig(output_path / f'{name}.png', dpi=dpi, bbox_inches='tight', **kwargs)
     plt.close()
     print(f'[X] Created plot for {name}.')
 
 
 if __name__ == '__main__':
     script_start_time = time.time()
+
     # Calculate max number of processes to allow for multiprocessing
-    mp_pool_count = max(mp.cpu_count() - 1, 1)
-    print(f'Using {mp_pool_count} cores for multiprocessing.')
+    _mp_pool_count = max(mp.cpu_count() - 1, 1)
+    print(f'Using {_mp_pool_count} cores for multiprocessing.')
 
     # Initialize Seaborn theme
     sns.set_theme()
@@ -162,13 +185,6 @@ if __name__ == '__main__':
     if not plot_path.exists():
         plot_path.mkdir()
 
-    # TODO: slower than single thread, presumably due to the overhead of multiprocessing and passing data between processes
-    # with mp.Pool(processes=mp_pool_count) as pool:
-    #     articles, customers, sample_submission, transactions_train = pool.map(load_data,
-    #                                                                           [articles_path, customers_path,
-    #                                                                            sample_submission_path,
-    #                                                                            transactions_train_path])
-
     articles = load_data(articles_path)
     customers = load_data(customers_path)
     sample_submission = load_data(sample_submission_path)
@@ -189,31 +205,28 @@ if __name__ == '__main__':
     plt.savefig(plot_path / 'sales_channel_id.png', dpi=400, bbox_inches='tight')
 
     # Plot relevant data as Seaborn plots
-    with mp.Pool(processes=mp_pool_count) as pool:
-        pool.starmap(create_plot, [(customers, 'age', plot_path),
-                                   (articles, 'product_type_name', plot_path, 0, 1, 5, 2, 25, 10, 75),
-                                   (articles, 'product_group_name', plot_path),
-                                   (articles, 'graphical_appearance_name', plot_path),
-                                   (articles, 'colour_group_name', plot_path, 0, 2, 5, 2, 15, 10, 90),
-                                   (articles, 'perceived_colour_value_name', plot_path),
-                                   (articles, 'perceived_colour_master_name', plot_path),
-                                   (articles, 'index_name', plot_path),
-                                   (articles, 'index_group_name', plot_path),
-                                   (articles, 'section_name', plot_path, 0, 1, 5, 2, 20, 10, 75),
-                                   (articles, 'garment_group_name', plot_path),
-                                   (articles, 'department_name', plot_path, 0, 1, 5, 2, 35, 10, 90),
-                                   (customers, 'club_member_status', plot_path),
-                                   (customers, 'fashion_news_frequency', plot_path)])
+    with mp.Pool(processes=_mp_pool_count) as _pool:
+        _pool.starmap(create_plot,
+                      [(customers, 'age', plot_path), (articles, 'product_type_name', plot_path, 0, 1, 5, 2, 25, 10, 75),
+                      (articles, 'product_group_name', plot_path), (articles, 'graphical_appearance_name', plot_path),
+                      (articles, 'colour_group_name', plot_path, 0, 2, 5, 2, 15, 10, 90),
+                      (articles, 'perceived_colour_value_name', plot_path),
+                      (articles, 'perceived_colour_master_name', plot_path), (articles, 'index_name', plot_path),
+                      (articles, 'index_group_name', plot_path),
+                      (articles, 'section_name', plot_path, 0, 1, 5, 2, 20, 10, 75),
+                      (articles, 'garment_group_name', plot_path),
+                      (articles, 'department_name', plot_path, 0, 1, 5, 2, 35, 10, 90),
+                      (customers, 'club_member_status', plot_path), (customers, 'fashion_news_frequency', plot_path)])
 
     # Describe data
     thread_start_time = time.time()
-    with mp.Pool(processes=mp_pool_count) as pool:
-        pool.starmap(describe_data, [(articles, 'articles', output_path, thread_start_time),
-                                     (customers, 'customers', output_path, thread_start_time),
-                                     (sample_submission, 'sample_submission', output_path, thread_start_time),
-                                     (transactions_train, 'transactions_train', output_path, thread_start_time)])
+    with mp.Pool(processes=_mp_pool_count) as _pool:
+        _pool.starmap(describe_data, [(articles, 'articles', output_path, thread_start_time),
+                                      (customers, 'customers', output_path, thread_start_time),
+                                      (sample_submission, 'sample_submission', output_path, thread_start_time),
+                                      (transactions_train, 'transactions_train', output_path, thread_start_time)])
 
     # Describe images
-    describe_images(images_path, output_path, mp_pool_count)
+    describe_images(images_path, output_path, _mp_pool_count)
 
     print(f'[X] Finished script in {time.time() - script_start_time:.2f} seconds.')
